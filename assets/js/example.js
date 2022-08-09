61d00d1a66995643f29d575821602c9774710839
Jatin_Saini <sainijatin247@gmail.com>
2022-08-09 12:20:24 -0400

    url2

--- assets/js/example.js
+++ assets/js/example.js
@@ -27,14 +27,19 @@ function cityHistory(cityName){
         console.log(cityArr);
 
         var li = $("<li>");
-        var currentId = li.attr("id");
+        //var currentId = li.attr("id",cityName);
         li.addClass("city-history");
         li.text(cityName);
         historyEl.append(li);
 
+
+
+        $("#cityName").on('click',function(){
+            console.log("click");
+            getApi(cityName);
+
+        })
 }
-
-
 
 
 
@@ -42,12 +47,22 @@ function getApi(cityName){
     var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&APPID=1e6eaae9d442e0e35ea4708556663762"
     fetch(url)
     .then(function(response){
-        return response.json();
+        if(response.ok){
+        return response.json()
+        .then(function(data){
+            var latitude = data.coord.lat;
+            var longitude = data.coord.lon;
+            console.log(latitude);
+            var tempEl = $(".tempData");
+            getInfo(data);
+            getInfo5(latitude,longitude);
+        
     })
-    .then(function(data){
-        var tempEl = $(".tempData");
+    }
+    // .then(function(data){
+    //     var tempEl = $(".tempData");
 
-        getInfo(data);
+        //getInfo(data);
         // console.log(data);
         // console.log(data.main.temp);
         // console.log(data.wind.speed);
@@ -75,3 +90,24 @@ function getInfo(data){
 
 
 }
+
+function getInfo5(latitude,longitude){
+
+    var url2 = "api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&units=metric&APPID=1e6eaae9d442e0e35ea4708556663762";
+    fetch(url2).then(function(response){
+        
+        return response.json()})
+        .then(function(data){
+
+            console.log(data.main.temp);
+            console.log(data.wind.speed);
+            console.log(data.main.humidity);
+
+                       
+            //var tempEl = $(".tempData");
+            //getInfo(data);
+            //getInfo5(latitude,longitude)
+
+       
+        })
+    }
